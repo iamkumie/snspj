79066b49e21cd718b01b7b81d0f08573f3fa666d
Kumie Yoshida <iamkumie@KumienoMacBook-Pro.local>
2021-07-06 12:45:47 +0900

    second-fix

--- pages/register.vue
+++ pages/register.vue
@@ -1,57 +1,70 @@
 <template>
-  <div class="register">
-    <h1>新規登録</h1>
-    <input v-model="username" type="text" placeholder="ユーザーネーム" required>
-    <br>
-    <input v-model="email" type="email" placeholder="メールアドレス" required>
-    <br>
-    <input v-model="password" type="password" placeholder="パスワード" required>
-    <br>
-    <button @click="register">新規登録</button>
+  <div
+    class="register"
+  >
+    <form
+      class="form"
+      @submit.prevent
+    >
+      <label class="label">
+        <span class="label">
+          email
+        </span>
+        <input
+          class="input"
+          type="text"
+          v-model="email"
+        >
+      </label>
+      <label class="label">
+        <span class="label">
+          password
+        </span>
+        <input
+          class="input"
+          type="password"
+          v-model="password"
+        >
+      </label>
+      <label class="label">
+        <span class="label">
+          name
+        </span>
+        <input
+          class="input"
+          type="text"
+          v-model="updateName"
+        >
+      </label>
+      <button
+        class="button"
+        type="submit"
+        @click="register"
+      >
+        Register
+      </button>
+    </form>
   </div>
 </template>
-
+ 
 <script>
-import firebase from '~/plugins/snspj'
 export default {
-  data() {
+  computed: {
+    user () {
+      return this.$store.getters['user']
+    },
+  },
+  data () {
     return {
-      username: null,
-      email: null,
-      password: null,
+      email: '',
+      password: '',
+      updateName: '',
     }
   },
   methods: {
-    register() {
-      if(!this.name || !this.email || !this.password) {
-        alert('ユーザーネームまたはメールアドレスまたはパスワードが入力されていません。')
-        return
-      }
-      firebase
-      .auth().createUserWithEmailAndPassword(this.email, this.password).then((data) => {
-        data.user.updateProfile({
-          displayName: this.username
-        }).then(() => {
-          this.$router.replace('/confirm')
-        })
-      })
-      .catch((error) => {
-        switch (error.code) {
-          case 'auth/invalid-email':
-            alert('メールアドレスの形式が違います。')
-            break
-          case 'auth/email-already-in-use':
-            alert('このメールドレスはすでに使われています。')
-            break
-          case 'auth/weak-password':
-            alert('パスワードは6文字以上で入力してください。')
-            break
-          default:
-            alert('エラーが起きました。しばらくしてから再度お試しください。')
-            break
-        }
-      })
+    register () {
+      this.$store.dispatch('register', {email: this.email, password: this.password, name: this.updateName})
     }
-  }
+  },
 }
 </script>
